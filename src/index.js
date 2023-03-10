ba81e6435810dec9c849b95587896d91e9cbdb7a
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-10 12:10:40 -0300

    :sparkles: processing and returning video on demand

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { spawn } from 'node:child_process'
 import { createReadStream } from 'node:fs'
 import { createServer, request } from 'node:http'
 
@@ -14,9 +15,29 @@ createServer(async (request, response) => {
     return;
   }
 
-  createReadStream('./assets/video.mp4').pipe(response)
+  response.writeHead(200, {
+    'Content-Type': 'video/mp4'
+  })
 
-  response.end("Hello World")
+  const ffmpeg_process = spawn('ffmpeg', [
+      '-i', 'pipe:0',
+      '-f', 'mp4',
+      '-vcodec', 'h264',
+      '-acodec', 'aac',
+      '-movflags', 'frag_keyframe+empty_moov+default_base_moof',
+      '-b:v', '1500k',
+      '-maxrate', '1500k',
+      '-bufsize', '1000k',
+      '-f', 'mp4',
+      'pipe:1'
+    ], { stdio: ['pipe', 'pipe', 'pipe'] }
+  )
+
+
+  createReadStream('./assets/video-ready.mp4').pipe(ffmpeg_process.stdin)
+
+  ffmpeg_process.stderr.on('data', msg => console.log(msg.toString()))
+  ffmpeg_process.stdout.pipe(response)
 })
 
 .listen(3000, () => console.log("Server Running at 3000"))
