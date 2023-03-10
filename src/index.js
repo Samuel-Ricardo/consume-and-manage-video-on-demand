68071f8c521f0f392e555f512727d4b595cdc153
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-10 10:31:25 -0300

    :gear: setup: cors / headers (index.js)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import { createServer, request } from 'node:http'
 
 createServer(async (request, response) => {
+    
+  const headers = {
+    'Access-Control-Allow-Origin': "*",
+    'Access-Control-Allow-Methods': "*",
+  }
+
+  if (request.method === 'OPTIONS') {
+    response.writeHead(204, headers)
+    response.end()
+    return;
+  }
+
   response.end("Hello World")
 })
 
