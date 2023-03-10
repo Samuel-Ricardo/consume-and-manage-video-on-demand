be40752826d344418966c81664fb86f48e550874
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-03-10 12:24:45 -0300

    :gear: update: script - prepare.sh (assets)

--- assets/prepare.sh
+++ assets/prepare.sh
@@ -3,7 +3,7 @@ ffmpeg \
   -vcodec h264                                         \  #video codec - most compatible
   -acodec aac                                          \  #audio codec - most compatible
   -movflags frag_keyframe+empty_moov+default_base_moof \  # multiples video fragments
-  -bv 1500k                                            \  #bitrate
+  -b:v 1500k                                           \  #bitrate
   -maxrate 1500k                                       \  #max bitrate
   -bufsize 1000k                                       \  #buffer size
   -f mp4                                               \  #output format
